c293690b9ef4b545ce841825ec12ea8a286bc011
xianglin <yxlyzxl@gmail.com>
2020-04-21 21:38:07 +0800

    implement the remaining part of this class

--- pure/RawFeatureGraph.py
+++ pure/RawFeatureGraph.py
@@ -4,7 +4,9 @@ __Auther__ = 'xianglin'
 
 import angr
 import networkx as nx
-from NumericFeatureExtractor import *
+import NumericFeatureExtractor
+import capstone
+# from NumericFeatureExtractor import *
 from tools.image import Image
 from tools.util.asm import is_jump
 
@@ -18,50 +20,87 @@ class RawFeatureGraph:
         raw_feature(list):[numerice_features, structual_features]
         _graph(nx.Digraph)
     """
-    def __init__(self, func_name, cfg):
+    def __init__(self, func_name, img, entry):
+        self.img = img
         self.func_name = func_name
-        self.cfg = cfg
-        self.raw_features = []
+        self.old_cfg = self.img.get_cfg(func_name)
+        self.g = nx.DiGraph()
+        self.entry = entry
+        self.init_graph()
 
-    ######################################################################
-    # structual features
-    ######################################################################
+    def __len__(self):
+        return len(self.g)
+
+    def init_graph(self):
+        """init all property for raw feature graph"""
+        self.old_cfg.normalize()
+        # add all nodes and edges to graph
+        print("start to init graph...")
+        for n in self.old_cfg.nodes():
+            if n.function_address == self.entry:
+                if n.block is not None:
+                    self.g.add_node(n.block)
+                    print("add {} block to graph...".format(hex(n.addr)))
+                    succ = n.successors
+                    for offs in succ:
+                        if offs.block is not None:
+                            self.g.add_edge(n.block, offs.block)
+        # add all features to every node
+        betweenness = nx.betweenness_centrality(self.g)
+        print("start to add properties to all blocks...")
+        for node in self.g.nodes():
+            string_consts, num_consts = NumericFeatureExtractor.get_BB_consts(self.img, node)
+            self.g.nodes[node]['str_consts'] = string_consts
+            self.g.nodes[node]['num_consts'] = num_consts
+            self.g.nodes[node]['call_insts'] = NumericFeatureExtractor.cal_call_insts(node)
+            self.g.nodes[node]['trans_insts'] = NumericFeatureExtractor.cal_transfer_insts(node)
+            self.g.nodes[node]['insts'] = NumericFeatureExtractor.cal_insts(node)
+            self.g.nodes[node]['arithmetic_insts'] = NumericFeatureExtractor.cal_arithmetic_insts(node)
+            self.g.nodes[node]['betweenness'] = betweenness[node]
+            self.g.nodes[node]['offs'] = self.g.out_degree(node)
+        print("init graph successfully...")
+
+    def feature_vec(self, block):
+        """retrive raw feature vector"""
+        feature_vec = []
+        # 0 string consts 10.82
+        feature_vec.append(self.g.nodes[block]['str_consts'])
+        # 1 numeric consts 14.47
+        feature_vec.append(self.g.nodes[block]['num_consts'])
+        # 2 transfer instruction num 6.54
+        feature_vec.append(self.g.nodes[block]['trans_insts'])
+        # 3 call instruction num 66.22
+        feature_vec.append(self.g.nodes[block]['call_insts'])
+        # 4 instruction num 41.37
+        feature_vec.append(self.g.nodes[block]['insts'])
+        # 5 arithmetic instruction num 55.65
+        feature_vec.append(self.g.nodes[block]['arithmetic_insts'])
+        # 6 offspring num 198.67
+        feature_vec.append(self.g.nodes[block]['offs'])
+        # 7 betweenness 30.66
+        feature_vec.append(self.g.nodes[block]['betweenness'])
+        return feature_vec
+
     @property
     def graph(self):
         """the networkx graph of cfg"""
-        if not hasattr(self, '_graph'):
-            g = nx.DiGraph()
-            # TODO
-            self._graph = g
-        return self._graph
-
-    @property
-    def betweeness(self):
-        """the betweeness centrality of a node in graph"""
-        if not hasattr(self, '_betweeness'):
-            betweenness = nx.betweenness_centrality(self.graph)
-            self._betweeness = betweenness
-        return self._betweeness
-
-    @property
-    def offspring(self):
-        """the outdegree of every node in graph"""
-        return []
-
-
+        return self.g
 
 
 def get_a_BB():
-    bin = "/home/xianglin/PycharmProjects/genius/testcase/2423496af35d94a87156b063ea5cedffc10a70a1/vmlinux"
-    # bin = "/home/xianglin/Graduation/executables/string_constant"
-    img = Image(bin)
-    # func_name = "main"
-    func_name = "dccp_rcv_state_process"
-    entry = 66664
-    func_cfg = img.get_cfg(func_name)
-    # img.project.loader.memory.load()
-    # bb = bin.factory.block(bin.entry)
-    gra = RawFeatureGraph(func_name, func_cfg)
+    debug_vmlinux = "../testcase/2423496af35d94a87156b063ea5cedffc10a70a1/vmlinux"
+    # debug_vmlinux="../testcase/x86_add"
+    # debug_vmlinux = "/home/xianglin/Graduation/executables/x64_a"
+    img = Image(debug_vmlinux)
+    # func_name = "dccp_rcv_state_process"
+    func_name = "show_stat"
+    entry_base = img.get_symbol_addr(func_name)
+    if not entry_base:
+        return
+    gra = RawFeatureGraph(func_name, img, entry_base)
+    for node in gra.graph.nodes():
+        node.pp()
+        print(gra.feature_vec(node))
     print(1)
 
 
