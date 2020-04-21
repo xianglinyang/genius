3d9ab9aa94bd67dac5cb4b2bd1748997f185d12c
xianglin <yxlyzxl@gmail.com>
2020-04-21 21:45:28 +0800

    add comments

--- pure/RawFeatureGraph.py
+++ pure/RawFeatureGraph.py
@@ -15,10 +15,11 @@ class RawFeatureGraph:
     """a graph to extract raw features of a cfg
 
     Attributes:
-        func_name(string)
-        cfg
-        raw_feature(list):[numerice_features, structual_features]
-        _graph(nx.Digraph)
+        img(tools.image.Image)
+        func_name(str)
+        old_cfg
+        g(networkx.Digraph)
+        entry(int): entry addr
     """
     def __init__(self, func_name, img, entry):
         self.img = img
